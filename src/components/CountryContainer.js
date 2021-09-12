4265d9370c113fd562b0e5b6a199df69dc987767
raduOarga <raduoarga95@gmail.com>
2021-09-12 13:49:42 +0200

    Fix country properties

--- src/components/CountryContainer.js
+++ src/components/CountryContainer.js
@@ -4,15 +4,17 @@ import CountryHeader from "./CountryHeader";
 import CountryProperty from "./CountryProperty";
 
 const CountryContainer = ({ countries }) => {
+  console.log("countries", countries);
+
   return (
     <div className={CountryContainerStyles.container}>
       <CountryHeader countries={countries} />
       <div className={CountryContainerStyles.properties}>
-        <CountryProperty text={"other names"}/>
-        <CountryProperty text={"region"}/>
-        <CountryProperty text={"borders"}/>
-        <CountryProperty text={"currencies"}/>
-        <CountryProperty text={"languages"}/>
+        <CountryProperty text={"other names"} countries={countries} property={countries.length > 0 ? countries[0].altSpellings : "Loading data..."}/>
+        <CountryProperty text={"region"} countries={countries}  property={countries.length > 0 ? countries[0].region : "Loading data..."}/>
+        <CountryProperty text={"borders"} countries={countries} property={countries.length > 0 ? countries[0].borders : "Loading data..."}/>
+        <CountryProperty text={"currencies"} countries={countries}  property={countries.length > 0 ? countries[0].currencies : "Loading data..."}/>
+        <CountryProperty text={"languages"} countries={countries} property={countries.length > 0 ? countries[0].languages : "Loading data..."}/>
       </div>
     </div>
   );
--- src/components/CountryProperty.js
+++ src/components/CountryProperty.js
@@ -1,19 +1,43 @@
-import React, {useState} from 'react';
-import CountryPropertyStyles from './CountryProperty.module.css';
+import React, { useState } from "react";
+import CountryPropertyStyles from "./CountryProperty.module.css";
+import Country from "./Pages/Country";
 
-const CountryProperty = ({text}) => {
-    const [visible, setVisible] = useState(false);
+const CountryProperty = ({ text, countries, property }) => {
+  const [visible, setVisible] = useState(false);
 
-    const clickHandler = () => {
-        setVisible(!visible);
-    }
+  const clickHandler = () => {
+    setVisible(!visible);
+  };
 
-    return (
-        <div className={CountryPropertyStyles.property}>
-            <button className={CountryPropertyStyles.name} onClick={clickHandler}>{text}</button>
-            <p className={`${CountryPropertyStyles.content} ${visible ? CountryPropertyStyles.showContent : CountryPropertyStyles.hideContent}`}>Bla bla</p>
-        </div>
-    )
-}
+  console.log("country", countries[0]);
+  console.log("property", property);
 
-export default CountryProperty
+  return (
+    <div className={CountryPropertyStyles.property}>
+      <button className={CountryPropertyStyles.name} onClick={clickHandler}>
+        {text}
+      </button>
+      <ul
+        className={`${CountryPropertyStyles.content} ${
+          visible
+            ? CountryPropertyStyles.showContent
+            : CountryPropertyStyles.hideContent
+        }`}
+      >
+        {countries.length < 1
+          ? property
+          : property === countries[0].currencies || property === countries[0].languages
+          ? property.map((element) => (
+              <li className={CountryPropertyStyles.listItem}>{element.name}</li>
+            ))
+          : Array.isArray(property)
+          ? property.map((element) => (
+              <li className={CountryPropertyStyles.listItem}>{element}</li>
+            ))
+          : property}
+      </ul>
+    </div>
+  );
+};
+
+export default CountryProperty;
