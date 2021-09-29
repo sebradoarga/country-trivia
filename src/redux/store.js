0c2ee6f6329d9b6a1c82b07ed94b36d36275d63e
raduOarga <raduoarga95@gmail.com>
2021-09-29 19:51:41 +0200

    Save state in localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
@@ -6,10 +6,30 @@ import rootReducer from "./reducers/index";
 
 const storeFactory = () => {
   const middlewares = [thunk];
+
+  const getLocalStorage = () => {
+    const persistedStateString = localStorage.getItem("persistedState");
+    const persistedState = persistedStateString
+      ? JSON.parse(persistedStateString)
+      : [];
+    console.log("first state", persistedState);
+
+    return persistedState;
+  };
+
   const reduxStore = createStore(
     rootReducer,
+    getLocalStorage(),
     composeWithDevTools(applyMiddleware(...middlewares))
   );
+
+  reduxStore.subscribe(() => {
+    localStorage.setItem(
+      "persistedState",
+      JSON.stringify(reduxStore.getState())
+    );
+  });
+
   return reduxStore;
 };
 
