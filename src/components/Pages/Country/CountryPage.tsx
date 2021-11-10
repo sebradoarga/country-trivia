c945aa0ae9fb003c607ca772dcb2261d1ac64d21
raduOarga <raduoarga95@gmail.com>
2021-11-10 15:59:12 +0100

    log data

--- src/components/Pages/Country/CountryPage.tsx
+++ src/components/Pages/Country/CountryPage.tsx
@@ -10,9 +10,11 @@ import { Country } from "../../../redux/types";
 
 const CountryPage = ({ windowWidth }: { windowWidth: number }) => {
   const { name } = useParams<{ name?: string }>();
+  console.log("name is", name);
   const country: Country[] = useSelector(
     (state: RootState) => state.countryReducer.country
   );
+  console.log("country is", country);
   const theme = useSelector((state: RootState) => state.themeReducer.theme);
 
   const dispatch = useDispatch();
